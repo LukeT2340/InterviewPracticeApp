f7f6e22f566cf724c2a8b00edd9e4f883c2d57ba
Luke Thompson <lukethompson220@gmail.com>
2024-02-08 14:17:59 +1100

    Initial Commit

---
+++ InterviewPractice/InterviewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewPracticeApp.swift
+//  InterviewPractice
+//
+//  Created by Luke Thompson on 8/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct InterviewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
